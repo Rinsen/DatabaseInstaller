afcb297f7c7f3a80a776cdba4f810e6766306d2c
Rinsen <fredrikh@outlook.com>
2015-08-10 23:50:03 +0200

    Added generic extension method for column builder to add foreign key

---
+++ src/Rinsen.DatabaseInstaller/Sql/Generic/ColumnBuilderExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Rinsen.DatabaseInstaller.Sql.Generic
+{
+    public static class ColumnBuilderExtensionMethods
+    {
+        public static ColumnBuilder ForeignKey<T>(this ColumnBuilder columnBuilder, Expression<Func<T, object>> propertyExpression)
+        {
+            var tableName = typeof(T).Name + "s";
+            var columnName = propertyExpression.GetMemberName();
+            columnBuilder.ForeignKey(tableName, columnName);
+            return columnBuilder;
+        }
+    }
+}
---
+++ test/Rinsen.DatabaseInstaller.Tests/Sql/Generic/ColumnBuilderExtensionMethodsTests.cs
@@ -0,0 +1,39 @@
+﻿using Rinsen.DatabaseInstaller.Sql.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Rinsen.DatabaseInstaller.Tests.Sql.Generic
+{
+    public class ColumnBuilderExtensionMethodsTests
+    {
+        public class Club
+        {
+            public int ClubId { get; set; }
+        }
+
+        public class ClubMember
+        {
+            public int Id { get; set; }
+
+            public int ClubId { get; set; }
+
+        }
+
+        [Fact]
+        public void AddForeignKeyConstraintOneOneColumnWithGenericExtensionMethod_GetCorrectInformationInTable()
+        {
+            // Arrange
+            var table = new Table<ClubMember>("ClubMembers");
+
+            // Act
+            table.AddAutoIncrementColumn(m => m.Id);
+            table.AddColumn(m => m.ClubId).ForeignKey<Club>(m => m.ClubId);
+
+            // Assert
+            Assert.Equal(1, table.Columns.Where(m => m.ForeignKey != null).Count());
+            Assert.Equal("ClubId", table.Columns.First(m => m.ForeignKey != null).ForeignKey.ColumnName);
+            Assert.Equal("Clubs", table.Columns.First(m => m.ForeignKey != null).ForeignKey.TableName);
+        }
+
+    }
+}
