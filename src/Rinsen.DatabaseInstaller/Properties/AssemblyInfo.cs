35978b977a22927d85ecd1ea8aa4e6966ac58624
Fredrik Rinsén <fredrikh@outlook.com>
2017-02-19 00:10:46 +0100

    Updated to CS proj and aspnet core 1.1

--- src/Rinsen.DatabaseInstaller/Properties/AssemblyInfo.cs
+++ src/Rinsen.DatabaseInstaller/Properties/AssemblyInfo.cs
@@ -14,6 +14,3 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e0e4bd27-d48e-470f-a7d5-56158943a7fd")]
