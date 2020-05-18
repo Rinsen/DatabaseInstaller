9d542e6436de5d894890fe48e7df7e2c43d42376
Fredrik Rinsén <fredrikh@outlook.com>
2020-05-18 22:21:33 +0200

    Improved installation console

---
+++ src/Database/DatabaseInstallerOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database
+{
+    public class DatabaseInstallerOptions
+    {
+        public string Path { get; set; }
+        public string AssemblyName { get; set; }
+        public string Command { get; set; }
+    }
+}
--- src/Database/Program.cs
+++ src/Database/Program.cs
@@ -10,207 +10,53 @@ using System.Linq;
 using System.Reflection;
 using System.Runtime.Loader;
 using System.Threading.Tasks;
+using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations;
 
 namespace Database
 {
     class Program
     {
-        static async Task Main(string[] args)
+        public static Task<int> Main(string[] args) => CommandLineApplication.ExecuteAsync<Program>(args);
+
+        [Argument(0, Description = "Command to execute")]
+        [Required]
+        public string Command { get; }
+
+        [Option("--ConnectionString", Description = "Db connection string")]
+        public string ConnectionString { get; }
+
+        [Option("--Path")]
+        public string Path{ get; }
+
+        [Option("--AssemblyName")]
+        public string AssemblyName { get; }
+
+        private async Task OnExecuteAsync()
         {
-            if (args.Length < 1)
-            {
-                Console.WriteLine("Some arguments is needed");
-                Console.ReadKey();
-            }
-            var path = @"C:\Users\fredr\Source\Repos\InnovationBoost\src\Rinsen.InnovationBoost.Installation\bin\Debug\netcoreapp3.1\publish";
-            var installationAssemblyName = "Rinsen.InnovationBoost.Installation.dll";
-
-            var installer = CreateInstaller();
-
-            switch (args[0].ToLower())
-            {
-                case "install":
-                    Console.WriteLine("Installing...");
-                    await Install(installer, path, installationAssemblyName);
-                    break;
-                case "preview":
-                    await PreviewDbChanges(installer, path, installationAssemblyName);
-                    break;
-                case "complete":
-                    AllDbChanges(path, installationAssemblyName);
-                    break;
-                case "current":
-                    await ShowCurrentInstallationState(installer);
-                    break;
-                default:
-                    Console.WriteLine("Some arguments is needed");
-                    Console.ReadKey();
-                    break;
-            }
-
-            Console.WriteLine($"Done with {args[0]}");
-            Console.ReadKey();
-        }
-
-        private static async Task ShowCurrentInstallationState(Installer installer)
-        {
-            Console.WriteLine("Installed versions");
-            Console.WriteLine("Id InstallationName InstalledVersion PreviousVersion StartedInstallatingVersion");
-            foreach (var installationNameAndVersion in await installer.GetVersionInformationAsync())
-            {
-                Console.WriteLine($"{installationNameAndVersion.Id} {installationNameAndVersion.InstallationName} {installationNameAndVersion.InstalledVersion} {installationNameAndVersion.PreviousVersion} {installationNameAndVersion.StartedInstallingVersion}");
-            }
-        }
-
-        private static async Task PreviewDbChanges(Installer installer, string path, string installationAssemblyName)
-        {
-            var dbChanges = GetAllDbChanges(path, installationAssemblyName);
-
-            var installationNamesAndVersion = await installer.GetVersionInformationAsync();
-
-            foreach (var installationName in dbChanges.Select(m => m.InstallationName).Distinct())
-            {
-                Console.WriteLine($"Installations for {installationName}");
-                Console.WriteLine();
-
-                var installationNameAndVersion = installationNamesAndVersion.SingleOrDefault(vi => vi.InstallationName == installationName);
-                var installationNameDbChanges = dbChanges.Where(dbc => dbc.InstallationName == installationName);
-
-                if (installationNameAndVersion != default(InstallationNameAndVersion))
+            var hostBuilder = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
                 {
-                    Console.WriteLine($"Version {installationNameAndVersion.InstalledVersion} of {installationNameDbChanges.Max(m => m.Version)} installed");
-
-                    foreach (var dbChange in installationNameDbChanges.Where(dbc => dbc.Version > installationNameAndVersion.InstalledVersion)
-                    .OrderBy(m => m.Version))
+                    services.AddDatabaseInstaller(ConnectionString);
+                    services.TryAdd(ServiceDescriptor.Singleton(typeof(ILogger<>), typeof(Logger<>)));
+                    services.AddHostedService<DatabaseInstallerHostedService>();
+                    services.AddTransient<DatabaseInstallationHandler>();
+                    services.AddSingleton(new DatabaseInstallerOptions
                     {
-                        PrintDbChange(dbChange);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"No previous version installed");
-
-                    foreach (var dbChange in installationNameDbChanges.OrderBy(m => m.Version))
+                        AssemblyName = AssemblyName,
+                        Command = Command,
+                        Path = Path
+                    });
+                    services.AddLogging(builder =>
                     {
-                        PrintDbChange(dbChange);
-                    }
-                }
-                Console.WriteLine("----------------------------------------------------------");
-                Console.WriteLine();
-            }
-        }
+                        builder.AddConsole();
+                    });
+                });
 
-        private static void PrintDbChange(DatabaseVersion dbChange)
-        {
-            Console.WriteLine($"Database change {dbChange.GetType().Name} version {dbChange.Version} ");
-            foreach (var command in dbChange.UpCommands)
-            {
-                Console.WriteLine(command);
-            }
-            Console.WriteLine();
-        }
+            var host = hostBuilder.Build();
 
-        private static void AllDbChanges(string path, string installationAssemblyName)
-        {
-            var dbChanges = GetAllDbChanges(path, installationAssemblyName);
-
-            foreach (var installationName in dbChanges.Select(m => m.InstallationName).Distinct())
-            {
-                Console.WriteLine($"Installations for {installationName}");
-                foreach (var dbChange in dbChanges.Where(dbc => dbc.InstallationName == installationName).OrderBy(m => m.Version))
-                {
-                    Console.WriteLine($"Version {dbChange.Version}");
-                    foreach (var command in dbChange.UpCommands)
-                    {
-                        Console.WriteLine(command);
-                    }
-                    Console.WriteLine();
-                }
-            }
-        }
-
-        private static async Task Install(Installer installer, string path, string installationAssemblyName)
-        {
-            var dbChangesToInstall = GetAllDbChanges(path, installationAssemblyName);
-
-            await installer.RunAsync(dbChangesToInstall);
-        }
-
-        private static Installer CreateInstaller()
-        {
-            var configBuilder = new ConfigurationBuilder();
-
-            configBuilder.AddUserSecrets<Program>();
-            //http://bleedingnedge.com/2015/10/15/configuration-providers/ Maybe add more or less?! Only args support? How to find assembly?
-
-            var config = configBuilder.Build();
-
-            var serviceCollection = new ServiceCollection();
-
-            serviceCollection.AddDatabaseInstaller(config["Data:DefaultConnection:ConnectionString"]);
-            serviceCollection.TryAdd(ServiceDescriptor.Singleton(typeof(ILogger<>), typeof(Logger<>)));
-
-            var serviceProvider = serviceCollection.BuildServiceProvider();
-
-            return serviceProvider.GetRequiredService<Installer>();
-        }
-
-        private static List<DatabaseVersion> GetAllDbChanges(string path, string installationAssemblyName)
-        {
-            Assembly assembly = null;
-            foreach (var file in Directory.EnumerateFiles(path).Where(f => f.EndsWith(".dll") && !f.EndsWith("Rinsen.DatabaseInstaller.dll")))
-            {
-                if (file.EndsWith(installationAssemblyName))
-                {
-                    assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(file);
-                }
-                else
-                {
-                    AssemblyLoadContext.Default.LoadFromAssemblyPath(file);
-                }
-            }
-
-            if (!(assembly is object))
-                throw new Exception($"Installation assembly {installationAssemblyName} not found at path {path}");
-
-            var dbChanges = new List<DatabaseVersion>();
-            foreach (var dbChange in assembly.ExportedTypes.Where(t => t.IsSubclassOf(typeof(DatabaseVersion))))
-            {
-                dbChanges.Add((DatabaseVersion)Activator.CreateInstance(dbChange));
-            }
-
-            return dbChanges;
-        }
-    }
-
-    class Logger<T> : ILogger<T>
-    {
-        public Logger()
-        {
-
-        }
-
-        public IDisposable BeginScope<TState>(TState state)
-        {
-            return new NoScope();
-        }
-
-        public bool IsEnabled(LogLevel logLevel)
-        {
-            return false;
-        }
-
-        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
-        {
-            
-        }
-
-        private class NoScope : IDisposable
-        {
-            public void Dispose()
-            {
-                
-            }
+            await host.RunAsync();
         }
     }
 }
